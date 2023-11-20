dd146ab91eb852829e0cf9cf3cd3cd8a8046b6db
Zoren Panilagao <89075806+Z3ron7@users.noreply.github.com>
2023-11-20 23:18:09 +0800

    Update Register.jsx

--- src/pages/Login/Register.jsx
+++ src/pages/Login/Register.jsx
@@ -124,6 +124,7 @@ export default function Register() {
         value={values.name}
         onChange={(e) => setValues({ ...values, name: e.target.value })}
         className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        require
       />
     </div>
     
@@ -137,6 +138,7 @@ export default function Register() {
         value={values.username}
         onChange={(e) => setValues({ ...values, username: e.target.value })}
         className="block w-full rounded-md py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        require
       />
     </div>
   </div>
@@ -176,6 +178,7 @@ export default function Register() {
     styles={customStyles}
     isSearchable
     placeholder="Select Gender"
+    require
   />
 </div>
 
@@ -239,6 +242,7 @@ export default function Register() {
     value={values.password}
     onChange={(e) => setValues({ ...values, password: e.target.value })}
     className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+    require
   />
   {values.password.length < 8 && values.password.length > 0 && (
     <p className="text-red-500 text-sm mt-1">Password must be at least 8 characters long.</p>
@@ -260,6 +264,7 @@ export default function Register() {
                   value={passwordConfirmation}
                   onChange={(e) => setPasswordConfirmation(e.target.value)}
                   className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  require
                 />
               </div>
             </div>

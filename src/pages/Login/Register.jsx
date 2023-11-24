734953183c30eb7994eda40d265f9a8235e6be4b
Zoren Panilagao <89075806+Z3ron7@users.noreply.github.com>
2023-11-24 08:54:58 +0800

    Update Register.jsx

--- src/pages/Login/Register.jsx
+++ src/pages/Login/Register.jsx
@@ -152,6 +152,7 @@ console.log("Backend Response:", response.data);
         className="block w-full rounded-md py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
         required
       />
+      
     </div>
   </div>
 
